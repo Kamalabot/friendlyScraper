15f4e64326b3c17b864809756725addd46a0f694
kamal raj <kamaljp@gmail.com>
2022-10-09 12:06:49 +0530

    started front end code

--- public/pagePopulater.js
+++ public/pagePopulater.js
@@ -1,105 +1,34 @@
-var getFile, putext;
-var askData, fileLoc,fileData
+//buttons
+var showLink, scrape, 
 
+//data show areas
+var dataOut, textOut, elemOut
 
-var itemIn, itemRet, setPriceB, priceIn, getPriceB,pricePrint,outComment;
-var socket = io();
-var chosenFileTxt;
-socket.on('mouse',newDrawing);
+//User inputs
 
-var fileType;
-
-function mousePressed(){
-    background(150,25,255)
-}
-
-function mouseDragged(){
-    fill(255,255,0)
-    circle(mouseX, mouseY, 25);
-    var data = {x:mouseX,y:mouseY};
-    socket.emit('mouse',data)
-}
-
-function newDrawing(data){
-    noStroke()
-    fill(255,0,255)
-    rect(data.x, data.y,25,35)
-}
+var link, span, list, links, paras, tables
 
 function setup(){
-    var canv = createCanvas(400,400)
-    .parent("canvas")
-    background(150,25,255)
-    loadJSON('/allPrice',gotData)
-    outComment = select('#outCom')
-    fileType = select('#filetype')
-    setPriceB = select('#setPrice')
+    noCanvas()
+    //buttons
+    showLink = select('#showLink')
+    scrape = select('#scrape')
 
-    getPriceB = select('#getPrice')
-    pricePrint = select('#outPrice')
+    //data areas
+    dataOut = select('#dataOut')
+    textOut = select('#textOut')
+    elemOut = select('#elemOut')
 
-    setPriceB.mouseClicked(setPriceData)
-    getPriceB.mouseClicked(getPriceData)
-    getFile = select('#getFile')
-    putext = select('#textArea')
+    showLink.mouseClicked(getOverview)
+    scrape.mouseClicked(getFullpage)
 
-    getFile.mouseClicked(loadFiles)
-
-    askData = select('#askData');
-    fileData = select('#fileData');
-    fileLoc = select('#fileLoc')
-
-    askData.mouseClicked(openAndSee)
+    span = createCheckbox
 }
 
-async function loadFiles(){
 
-    var fetchList = await fetch('listfile/')
-    var listData = await fetchList.json();
-    print(`This is fetch:${listData}`)
-
-    var arrayFiles = Object.entries(listData)
-    putext.html('')
-    for(let elt of arrayFiles){
-        createElement('span')
-            .parent('textArea')
-            .html(`${elt[0]} : ${elt[1]}`)
-        createElement('br')
-            .parent('textArea')
-    }
-}
-//you have to recieve the JSON only.
-async function openAndSee(){
-    var filePath = fileLoc.value();
-    print(filePath)
-    var fetchText = await fetch(`uploads/${filePath}`)
-    var data = await fetchText.json()
-    // var fetchText = httpGet(`uploads/${filePath}`,(data,err)=>{
-    print(data.text)
-    fileData.html(data.text)
+function getOverview(){
     
-}
-
-
-function gotData(data){
-    console.log(data)
-}
-function setPriceData(){
-    itemIn = select('#item').value()
-    priceIn = select('#price').value()
-    print('enter set')   
-    
-    if(itemIn !='' && priceIn !=''){
-        loadJSON(`/add/${itemIn}/${priceIn}`)
-        outComment.html('price is registered...')
-    }else{
-        outComment.html('Please enter both item and price')
-    } 
-}
-
-function getPriceData(){
-    print('enter get')
-    itemRet = select('#itemRet').value()   
+    link = select('#linkScrape').value()   
     
     if(itemRet){
         loadJSON('/allPrice', (data)=>{
